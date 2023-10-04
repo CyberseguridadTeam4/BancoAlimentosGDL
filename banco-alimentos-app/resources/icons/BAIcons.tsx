ddc4b65a1da4d3436036f2eb02015a4ecefaf8b5
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-10-03 18:52:30 -0600

    [Resources] Added more Icons

--- banco-alimentos-app/resources/icons/BAIcons.tsx
+++ banco-alimentos-app/resources/icons/BAIcons.tsx
@@ -1,28 +1,40 @@
-import {ImageSourcePropType} from 'react-native';
+import { ImageSourcePropType } from "react-native";
 
 export default {
   get AddIcon(): ImageSourcePropType {
-    return require('./AddIcon.png');
+    return require("./AddIcon.png");
   },
   get BackIcon(): ImageSourcePropType {
-    return require('./BackIcon.png');
+    return require("./BackIcon.png");
   },
   get CrossIcon(): ImageSourcePropType {
-    return require('./CrossIcon.png');
+    return require("./CrossIcon.png");
   },
   get LockIcon(): ImageSourcePropType {
-    return require('./LockIcon.png');
+    return require("./LockIcon.png");
   },
   get PersonIcon(): ImageSourcePropType {
-    return require('./PersonIcon.png');
+    return require("./PersonIcon.png");
   },
   get PlayIcon(): ImageSourcePropType {
-    return require('./PlayIcon.png');
+    return require("./PlayIcon.png");
   },
   get SearchIcon(): ImageSourcePropType {
-    return require('./SearchIcon.png');
+    return require("./SearchIcon.png");
   },
   get UnlockIcon(): ImageSourcePropType {
-    return require('./UnlockIcon.png');
+    return require("./UnlockIcon.png");
+  },
+  get MapIcon(): ImageSourcePropType {
+    return require("./MapIcon.png");
+  },
+  get BirdIcon(): ImageSourcePropType {
+    return require("./BirdIcon.png");
+  },
+  get ForoIcon(): ImageSourcePropType {
+    return require("./ForoIcon.png");
+  },
+  get SettingIcon(): ImageSourcePropType {
+    return require("./SettingIcon.png");
   },
 };
