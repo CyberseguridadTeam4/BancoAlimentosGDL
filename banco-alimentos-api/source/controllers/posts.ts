6f793b3ee14f5497de85a698983cb6dc84e12fbf
BoJavs <javiereric1309@gmail.com>
2023-10-04 18:10:07 -0600

    [Backend] User creation without email of password verification and User GET function

--- banco-alimentos-api/source/controllers/posts.ts
+++ banco-alimentos-api/source/controllers/posts.ts
@@ -16,8 +16,7 @@ if (appID && jsKey) {
 // Create user in database
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {name,nextStage}=req.body
-        // Example: Creating a new object
+        const { username, password, email, name, nextStage } = req.body; 
         const Pollo=Parse.Object.extend('Pollo');
         const pollo = new Pollo();
     
@@ -27,6 +26,32 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
         // Save the object to the database
         await pollo.save();
+
+        // Create a new Parse User object
+        const User = Parse.Object.extend('_User');
+        const user = new User();
+
+        // Set properties for the User object
+        user.set('username', username);
+        user.set('password', password);
+        user.set('email', email);
+
+        // Set additional properties for the User object
+        
+        //////////////////////////////
+        user.set('emailVerified', false); // Email verification status
+        //////////////////////////
+
+        user.set('badges', []); // Initialize badges as an empty array
+        user.set('idProfilePicture', 0); // Initialize profile picture ID 0 is starting picture for everyone
+        user.set('visBadge', -1); // Initialize visibility badge. -1 means no badges
+
+        // Create a pointer to the Pollo object
+        const polloPointer = Pollo.createWithoutData(pollo.id); // 
+        user.set('pollo', polloPointer);
+
+        // Save the User object to the database
+        await user.save();
 
         return res.status(200).json({
             message: 'New object created successfully',
@@ -39,4 +64,25 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 };
 
-export default { createUser };
+// Get user by objectId
+const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { userId } = req.params; // Extract the userId from the URL parameters
+
+        // Create a new Parse Query for the "_User" class
+        const User = Parse.Object.extend('_User');
+        const query = new Parse.Query(User);
+
+        // Use the objectId to find the user
+        const user = await query.get(userId);
+
+        return res.status(200).json(user);
+    } catch (error) {
+        console.log('Error fetching user: ', error);
+        return res.status(500).json({
+            message: 'Error fetching user',
+        });
+    }
+};
+
+export default { createUser ,getUserById};
--- banco-alimentos-api/source/routes/posts.ts
+++ banco-alimentos-api/source/routes/posts.ts
@@ -2,7 +2,8 @@
 import express from 'express';
 import controller from '../controllers/posts';
 const router = express.Router();
-
+//User
+router.get('/user/:userId', controller.getUserById);
 router.post('/user', controller.createUser);
 
 
