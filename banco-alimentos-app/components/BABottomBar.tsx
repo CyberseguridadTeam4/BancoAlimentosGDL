0190a97acada38336aeef2756da2a7e4176099ff
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-10-10 09:36:19 -0600

    [UI][BottomBar] Added Responsive styles

--- banco-alimentos-app/components/BABottomBar.tsx
+++ banco-alimentos-app/components/BABottomBar.tsx
@@ -1,4 +1,9 @@
-import { View, StyleSheet, ImageSourcePropType } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ImageSourcePropType,
+  SafeAreaView,
+} from "react-native";
 import React, { Component } from "react";
 import Svg, { Path } from "react-native-svg";
 import BAPallete from "../resources/BAPallete";
@@ -17,7 +22,7 @@ const BUTTONS_STYLES: ImageSourcePropType[] = [
 export default class BABottomBar extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <View style={styles.buttonContainer}>
           {BUTTONS_STYLES.map((item, index) => {
             return (
@@ -33,13 +38,13 @@ export default class BABottomBar extends Component {
             );
           })}
         </View>
-        <Svg height="100" width="500">
+        <Svg height="100vh" width="500">
           <Path
             d="M0 0 L195 0 a10,10 0 0 1 10,10 L205 40 a10,10 0 0 0 10,10 L285 50  a10,10 0 0 0 10,-10 L295 10 a10,10 0 0 1 10,-10 L500 0 L500 100 L0 100 Z"
             fill={BAPallete.Red01}
           />
         </Svg>
-      </View>
+      </SafeAreaView>
     );
   }
 }
@@ -48,29 +53,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     position: "absolute",
-    bottom: 0,
+    bottom: "-4%",
     alignItems: "center",
     justifyContent: "center",
   },
   buttonContainer: {
-    width: "100%",
-    aspectRatio: 1 / 1,
+    flex: 1,
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     position: "absolute",
     alignContent: "center",
-    top: -15,
+    top: "-15%",
     zIndex: 10,
-    paddingHorizontal: 35,
+    paddingHorizontal: "13%",
   },
   buttons: {
-    flex: 0,
-    width: 70,
-    margin: 15,
+    marginTop: 15,
     aspectRatio: 1 / 1,
   },
   middleButton: {
     top: -30,
-    marginHorizontal: 15,
+    marginHorizontal: "5%",
   },
 });
