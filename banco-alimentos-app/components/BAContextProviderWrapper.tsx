edc898de8adf1adebd4e8e72f61f72ec4faa4504
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-10-11 18:07:51 -0600

    [UI][Toast[ Added Toast Provider

--- banco-alimentos-app/components/BAContextProviderWrapper.tsx
+++ banco-alimentos-app/components/BAContextProviderWrapper.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { SheetProvider } from "./Sheet/BASheetContext";
 import { ModalProvider } from "./Modal/BAModalContext";
+import { ToastProvider } from "./Toast/BAToastContext";
 
 export default function BAContextProviderWrapper({ children }: any) {
   return (
     <SheetProvider>
-      <ModalProvider>{children}</ModalProvider>
+      <ToastProvider>
+        <ModalProvider>{children}</ModalProvider>
+      </ToastProvider>
     </SheetProvider>
   );
 }
