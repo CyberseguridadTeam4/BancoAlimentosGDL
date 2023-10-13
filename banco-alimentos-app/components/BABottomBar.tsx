8f75bb191ecf9a41ce9a1fa70a4955d5db63cc41
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-10-13 09:52:18 -0600

    [UI][BottomBar] Added Animation when Toaster is opening

--- banco-alimentos-app/components/BABottomBar.tsx
+++ banco-alimentos-app/components/BABottomBar.tsx
@@ -3,14 +3,17 @@ import {
   StyleSheet,
   ImageSourcePropType,
   SafeAreaView,
+  Animated,
+  Easing,
 } from "react-native";
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useRef, useState } from "react";
 import Svg, { Path } from "react-native-svg";
 import BAPallete from "../resources/BAPallete";
 import BAButton, { ButtonState } from "./BAButton";
 import BAIcons from "../resources/icons/BAIcons";
 import { IconSize } from "../resources/icons/BAIcon";
 import BAToastController from "./Toast/BAToast";
+import { useToast } from "./Toast/BAToastContext";
 
 const BUTTONS_STYLES: ImageSourcePropType[][] = [
   [BAIcons.ForoIcon, BAIcons.ForoActivatedIcon],
@@ -28,6 +31,7 @@ export default function BABottomBar() {
         {BUTTONS_STYLES.map((item: ImageSourcePropType[], index: number) => {
           return index == 2 ? (
             <MiddleButton
+              key={index}
               index={index}
               optionSelected={optionSelected}
               setOptionSelected={setOptionSelected}
@@ -58,22 +62,67 @@ export default function BABottomBar() {
 }
 
 const MiddleButton = ({ index, optionSelected, setOptionSelected }: any) => {
+  const buttonScale = useRef(new Animated.ValueXY({ x: 1, y: 1 })).current;
+  const [playAnimation, setPlayAnimation] = useState(true);
+
+  const { showToast, openToast } = useToast();
+
+  useEffect(() => {
+    setPlayAnimation(!showToast);
+    if (playAnimation) {
+      Animated.sequence([
+        Animated.timing(buttonScale, {
+          toValue: { x: 1, y: 1 },
+          duration: 50,
+          easing: Easing.ease,
+          useNativeDriver: true,
+        }),
+        Animated.timing(buttonScale, {
+          toValue: { x: 0.8, y: 1.2 },
+          duration: 200,
+          easing: Easing.ease,
+          useNativeDriver: true,
+        }),
+        Animated.timing(buttonScale, {
+          toValue: { x: 1.2, y: 0.8 },
+          duration: 100,
+          easing: Easing.ease,
+          useNativeDriver: true,
+        }),
+        Animated.timing(buttonScale, {
+          toValue: { x: 1, y: 1 },
+          duration: 150,
+          easing: Easing.ease,
+          useNativeDriver: true,
+        }),
+      ]).start();
+    }
+  }, [openToast]);
+
   return (
-    <View style={styles.middleButtonWrapper}>
-      <BAToastController></BAToastController>
-      <BAButton
-        onPress={() => {
-          setOptionSelected(index);
-        }}
-        icon={
-          optionSelected == index ? BAIcons.BirdActivatedIcon : BAIcons.BirdIcon
-        }
-        iconSize={IconSize.large}
-        state={ButtonState.alert}
-        style={styles.middleButton}
-        disableShadow={true}
-      />
-    </View>
+    <Animated.View
+      style={[
+        styles.middleButtonWrapper,
+        { transform: [{ scaleX: buttonScale.x }, { scaleY: buttonScale.y }] },
+      ]}
+    >
+      <BAToastController />
+      <View style={styles.middleButton}>
+        <BAButton
+          onPress={() => {
+            setOptionSelected(index);
+          }}
+          icon={
+            optionSelected == index
+              ? BAIcons.BirdActivatedIcon
+              : BAIcons.BirdIcon
+          }
+          iconSize={IconSize.large}
+          state={ButtonState.alert}
+          disableShadow={true}
+        />
+      </View>
+    </Animated.View>
   );
 };
 
