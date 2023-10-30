2f48398bf7ca4b765ac011fc24adc9441c36a1b1
Mar-bh <marianabhdez@gmail.com>
2023-10-30 09:28:53 -0600

    [UI][MapView] Implementation of map styles

--- banco-alimentos-app/views/BAMapView.tsx
+++ banco-alimentos-app/views/BAMapView.tsx
@@ -1,26 +1,47 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View,  Dimensions } from "react-native";
 import React, { useState } from "react";
 import BAView from "../components/BAView";
 import BATextInput from "../components/BATextInput";
 import BAButton, { ButtonState } from "../components/BAButton";
 import BAIcons from "../resources/icons/BAIcons";
-import { IconSize,  } from "../resources/icons/BAIcon";
+import { IconSize } from "../resources/icons/BAIcon";
+//import Constants from 'expo-constants';
+
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import BAText from "../components/BAText";
+
+//const apiKey = Constants.extra.googleMapsApiKey;
 
 export default function BAMapView(){
     const [place, setPlace] = useState("");
     return (
-        <BAView title="Map" isScrolling={true}>
+        <BAView title="Map" isScrolling={true}  style={styles.body}>
+          <View style = {styles.mapContainer}>
+            {/* <BAText> prueba</BAText> */}
+            <MapView
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            />
+          </View>
             <View style={styles.buttonContainer}>
                 <View style={styles.inputContainer}>
                     <BATextInput value={place} onChange={setPlace} placeholder="Find place..."/> 
                 </View> 
                 <BAButton onPress={() =>{}} icon={BAIcons.SearchIcon} iconSize={IconSize.large} style={styles.buttons} />
-            </View>
+            </View>  
         </BAView>
     );
 }
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
+      body: {
+        flex: 1,
+        alignContent: "center",
+        justifyContent: "center",
+    },
     buttons: {
         flex: 1,
         marginLeft:  20,
@@ -30,9 +51,20 @@ const styles = StyleSheet.create({
         width: "100%",
         flexDirection: "row",
         justifyContent: "space-between",
+        marginTop: 20,
       },
       inputContainer: {
         width: "80%",
         height: "100%",
-      }
+      },
+      mapContainer: {
+        flex: 1,
+        // backgroundColor: "pink",
+        // marginBottom: 40,
+        height: windowHeight/2,  // Half of the window height
+      },
+      map: {
+        width: '100%',
+        height: '100%',
+      },
 });
