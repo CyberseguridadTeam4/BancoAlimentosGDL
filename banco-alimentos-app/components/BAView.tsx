6ff4f6d6a92a6ec0576b6dbe1c904f6d85b8d276
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-11-01 09:52:24 -0600

    [Views][Component] Added Refresh function & Fixed Keyboard Avoiding

--- banco-alimentos-app/components/BAView.tsx
+++ banco-alimentos-app/components/BAView.tsx
@@ -5,6 +5,9 @@ import {
   StyleProp,
   ViewStyle,
   SafeAreaView,
+  KeyboardAvoidingView,
+  Platform,
+  RefreshControlProps,
 } from "react-native";
 import React from "react";
 import BAText, { TypeText } from "./BAText";
@@ -14,13 +17,20 @@ type ViewProps = {
   title: string;
   style?: StyleProp<ViewStyle>;
   isScrolling?: boolean;
+  onRefresh?:
+    | React.ReactElement<
+        RefreshControlProps,
+        string | React.JSXElementConstructor<any>
+      >
+    | undefined;
 };
 
 export default function BAView({
   children,
   style,
   title,
-  isScrolling = false,
+  isScrolling = true,
+  onRefresh,
 }: ViewProps) {
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -35,17 +45,26 @@ export default function BAView({
       >
         {title}
       </BAText>
-      <ScrollView
-        scrollEnabled={isScrolling}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={
-          isScrolling
-            ? { paddingHorizontal: 20, paddingTop: 20 }
-            : styles.container
-        }
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 25 : undefined}
+        style={{ flex: 1 }}
       >
-        <View style={style}>{children}</View>
-      </ScrollView>
+        <ScrollView
+          scrollEnabled={isScrolling}
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{
+            paddingHorizontal: 20,
+            paddingTop: 20,
+            paddingBottom: 100,
+            flexGrow: 1,
+          }}
+          refreshControl={onRefresh}
+        >
+          <View style={style}>{children}</View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -54,7 +73,7 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     paddingHorizontal: 20,
-    marginBottom: 150,
+    paddingBottom: 100,
     paddingTop: 20,
   },
 });
