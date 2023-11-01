6f003997b3853c0ae53f647a6d0e9cc682983ff6
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-11-01 09:56:11 -0600

    [Files] Moved Views TSX to Views Folder

--- banco-alimentos-app/App.tsx
+++ banco-alimentos-app/App.tsx
@@ -9,7 +9,7 @@ import BASheetController from "./components/Sheet/BASheet";
 import BAToastController from "./components/Toast/BAToast";
 import BAPostsView from "./views/BAPostsView";
 import BABird from "./components/BABird";
-import BAAcount from "./components/BAAccountView";
+import BAAcount from "./views/BAAccountView";
 
 export default function App() {
   const [selectedOption, setSelectedOption] = useState("1");
