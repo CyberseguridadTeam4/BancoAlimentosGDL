ea3654bc11d4de3d6c6c23aa1eec78d7e4a254c6
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-11-09 14:16:09 -0600

    [Settings][Code] Fixed fucntion Component

--- banco-alimentos-app/views/BASettingsView.tsx
+++ banco-alimentos-app/views/BASettingsView.tsx
@@ -5,43 +5,41 @@ import BAView from "../components/BAView";
 import BAButton, { ButtonState } from "../components/BAButton";
 import BAText from "../components/BAText";
 
-export default class BASettingsView extends Component {
-  render() {
-    return (
-      <BAView title="Configuración" style={styles.body}>
-        <View style={styles.buttonsWrapper}>
-          <BAButton text="Términos y Condiciones" onPress={() => {}} />
-          <BAButton text="Cerrar Sesión" onPress={() => {}} />
-        </View>
-        <BAButton
-          text="Eliminar Cuenta"
-          state={ButtonState.alert}
-          onPress={() => {}}
-          style={{ marginVertical: 20 }}
-        />
-        <Image
-          source={require("../assets/AppIcon.png")}
-          style={styles.imageStyle}
-        />
-        <View style={styles.membersWrapper}>
-          <BAText style={{ marginBottom: 10 }}>Equipo:</BAText>
-          <BAText>Mariana Esquivel Hernandez</BAText>
-          <BAText>Javier Eric Garza Hernandez</BAText>
-          <BAText>Mariana Bustos Hernandez</BAText>
-          <BAText>Israel Vidal Paredes</BAText>
-          <BAText>Iker Ochoa Villaseñor</BAText>
-        </View>
-        <Image
-          source={require("../assets/TecIcon.png")}
-          style={styles.imageStyle}
-        />
-        <Image
-          source={require("../assets/BALogo.png")}
-          style={styles.imageStyle}
-        />
-      </BAView>
-    );
-  }
+export default function BASettingsView() {
+  return (
+    <BAView title="Configuración" style={styles.body}>
+      <View style={styles.buttonsWrapper}>
+        <BAButton text="Términos y Condiciones" onPress={() => {}} />
+        <BAButton text="Cerrar Sesión" onPress={() => {}} />
+      </View>
+      <BAButton
+        text="Eliminar Cuenta"
+        state={ButtonState.alert}
+        onPress={() => {}}
+        style={{ marginVertical: 20 }}
+      />
+      <Image
+        source={require("../assets/AppIcon.png")}
+        style={styles.imageStyle}
+      />
+      <View style={styles.membersWrapper}>
+        <BAText style={{ marginBottom: 10 }}>Equipo:</BAText>
+        <BAText>Mariana Esquivel Hernandez</BAText>
+        <BAText>Javier Eric Garza Hernandez</BAText>
+        <BAText>Mariana Bustos Hernandez</BAText>
+        <BAText>Israel Vidal Paredes</BAText>
+        <BAText>Iker Ochoa Villaseñor</BAText>
+      </View>
+      <Image
+        source={require("../assets/TecIcon.png")}
+        style={styles.imageStyle}
+      />
+      <Image
+        source={require("../assets/BALogo.png")}
+        style={styles.imageStyle}
+      />
+    </BAView>
+  );
 }
 
 const styles = StyleSheet.create({
