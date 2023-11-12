9b178051c46e3c8667760c5404f8e0f1e68d7688
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-11-12 14:22:12 -0600

    [Loading][Views] Created Loading Context

--- banco-alimentos-app/components/BAContextProviderWrapper.tsx
+++ banco-alimentos-app/components/BAContextProviderWrapper.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { SheetProvider } from "./Sheet/BASheetContext";
 import { ModalProvider } from "./Modal/BAModalContext";
 import { ToastProvider } from "./Toast/BAToastContext";
+import { LoadingProvider } from "./Loading/BALoadingContext";
 
 export default function BAContextProviderWrapper({ children }: any) {
   return (
-    <SheetProvider>
-      <ToastProvider>
-        <ModalProvider>{children}</ModalProvider>
-      </ToastProvider>
-    </SheetProvider>
+    <LoadingProvider>
+      <SheetProvider>
+        <ToastProvider>
+          <ModalProvider>{children}</ModalProvider>
+        </ToastProvider>
+      </SheetProvider>
+    </LoadingProvider>
   );
 }
