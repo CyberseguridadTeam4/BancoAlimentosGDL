3629cedec30ac543e796f2961ee759672702b930
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-11-16 20:46:03 -0600

    [User][Context] Createdf user context to manage data

--- banco-alimentos-app/components/BAContextProviderWrapper.tsx
+++ banco-alimentos-app/components/BAContextProviderWrapper.tsx
@@ -3,15 +3,18 @@ import { SheetProvider } from "./Sheet/BASheetContext";
 import { ModalProvider } from "./Modal/BAModalContext";
 import { ToastProvider } from "./Toast/BAToastContext";
 import { LoadingProvider } from "./Loading/BALoadingContext";
+import { UserProvider } from "./BAUserContext";
 
 export default function BAContextProviderWrapper({ children }: any) {
   return (
-    <LoadingProvider>
-      <SheetProvider>
-        <ToastProvider>
-          <ModalProvider>{children}</ModalProvider>
-        </ToastProvider>
-      </SheetProvider>
-    </LoadingProvider>
+    <UserProvider>
+      <LoadingProvider>
+        <SheetProvider>
+          <ToastProvider>
+            <ModalProvider>{children}</ModalProvider>
+          </ToastProvider>
+        </SheetProvider>
+      </LoadingProvider>
+    </UserProvider>
   );
 }
