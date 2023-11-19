a29c781de861fc18ec441e5e55b4fb1be93ffaf6
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-11-18 20:45:38 -0600

    [Posts][User] Added message when user has not post posted

--- banco-alimentos-app/views/BAMisPosts.tsx
+++ banco-alimentos-app/views/BAMisPosts.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import { useLoading } from "../components/Loading/BALoadingContext";
 import { Post } from "./BAPostsView";
+import BAText, { TypeText } from "../components/BAText";
 
 export default function BAMisPosts({ setOnPostPress, setPostPressed }: any) {
   const [posts, setPosts] = useState([]);
@@ -22,7 +23,7 @@ export default function BAMisPosts({ setOnPostPress, setPostPressed }: any) {
   return (
     <>
       <View style={styles.postsContainer}>
-        {posts.length > 0 &&
+        {posts.length > 0 ? (
           posts.map((post: any) => {
             return (
               <View key={post.objectId}>
@@ -37,7 +38,23 @@ export default function BAMisPosts({ setOnPostPress, setPostPressed }: any) {
                 />
               </View>
             );
-          })}
+          })
+        ) : (
+          <>
+            <BAText
+              type={TypeText.label3}
+              style={{ textAlign: "center", marginTop: 150 }}
+            >
+              No tienes posts publicados aún.
+            </BAText>
+            <BAText type={TypeText.label3} style={{ textAlign: "center" }}>
+              Intenta presionando "+" en la sección Posts.
+            </BAText>
+            <BAText type={TypeText.label3} style={{ textAlign: "center" }}>
+              Puede que obtengas alguna recompenza interesante.
+            </BAText>
+          </>
+        )}
       </View>
     </>
   );
