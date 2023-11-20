1b1d5b1b364c282d1e7ebf1fb160130462507d45
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-11-20 11:52:55 -0600

    [Toast] Fixed duplicated Toast

--- banco-alimentos-app/App.tsx
+++ banco-alimentos-app/App.tsx
@@ -46,7 +46,6 @@ export function AppContent() {
         )}
         <BASheetController />
         <BAModalController />
-        <BAToastController />
       </BAContextProviderWrapper>
     </View>
   );
